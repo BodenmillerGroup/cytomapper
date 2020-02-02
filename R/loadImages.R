4b74daf5e26443d94960d91f3b8388d0becc78dd
nilseling <nils.eling@gmx.de>
2020-02-02 19:09:28 +0100

    Finished loadImage function

--- R/loadImages.R
+++ R/loadImages.R
@@ -40,100 +40,28 @@
 #' @author Nils Eling \email{nils.eling@@dqbm.uzh.ch},
 #' Nicolas Damond \email{nicolas.damond@@dqbm.uzh.ch}
 #'
-#' @importFrom EBImage Image
-#' @importFrom tools file_ext
+#' @importFrom EBImage Image readImage
 #' @export
 loadImages <- function(x, pattern = NULL, ...) {
 
   # Validity checks
   x <- .valid.loadImage.input(x, pattern)
 
-  # If an external csv file is provided, import it
-  if(!is.null(image.list) & is.character(image.list)){
-    image.list <- importCSV(image.list)
+  # Read in images
+  if(length(x) == 1){
+    out <- EBImage::readImage(x, ...)
+
+  } else {
+    cur_list <- lapply(x, function(y){EBImage::readImage(y, ...)})
+    out <- ImageList(cur_list)
+    names(out) <- sub("\\.[^.]*$", "", basename(x))
   }
 
-  # Create a SimpleList containing image paths, names and ids
-  image.list <- createImageList(path=image.path, name=image.name, id=image.id, image.list=image.list)
-
-  # Read the images with EBImage
-  images <- readImages(image.list)
-
-  return(images)
-}
-
-#' readImages
-#'
-#' read the images and store them in an ImageList (currently, a SimpleList, should be adapted)
-#'
-#' @param image.list
-#' @keywords image read
-#' @return images
-#' @import
-#' @importFrom EBImage readImage
-#' @export
-
-readImages <- function (image.list) {
-  fn <- file.path(image.list$imagePath, image.list$imageName)
-
-  SimpleList(images)
-  images <- lapply(fn, EBImage::readImage)
+  return(out)
 }
 
 
-
-#' createImageList
-#'
-#' Store the image paths and names in a DataFrame
-#'
-#' @param path
-#' @param name
-#' @param id
-#' @param image.list
-#' @keywords image list path
-#' @return image.list
-#' @import
-#' @importFrom S4Vectors DataFrame
-#' @export
-
-createImageList <- function (path, name, id, image.list=NULL) {
-
-  if(is(image.list, "SingleCellExperiment")){
-    image.list <- colData(sce)
-  }
-
-  if(is.null(image.list)){
-    image.list <- DataFrame(imagePath = path,
-                            imageName = name,
-                            imageID = id)
-  } else if (!is.null(image.list)){
-    image.list <- DataFrame(imagePath = image.list[, (path)],
-                            imageName = image.list[, (name)],
-                            imageID = image.list[, (id)])
-  }
-  return(unique(image.list))
-}
-
-
-#' importCSV
-#'
-#' If an external CSV file is provided, import it
-#'
-#' @param image.list
-#' @keywords image list csv import
-#' @return image.list
-#' @import
 #' @importFrom tools file_ext
-#' @export
-
-#importCSV <- function (image.list) {
-
-#  if (file.exists(image.list) & tools::file_ext(image.list) == "csv")
-#    image.list <- read.csv(image.list, header = T, stringsAsFactors = F)
-#
-#  return(image.list)
-#}
-
 .valid.loadImage.input <- function(x, pattern){
 
   # Check if input is character
