746c4ff045161ffdc8e46c01730b13bf67c5917c
nilseling <nils.eling@gmx.de>
2020-02-04 12:38:34 +0100

    Example for datasets

---
+++ R/data.R
@@ -0,0 +1,24 @@
+#' Example SingleCellExperiment object
+#'
+#' TODO
+#'
+#' @format A SingleCellExperiment object containing ...
+#' \describe{
+#'   \item{colData}{cell-level metadata...}
+#'   \item{rowData}{marker-level metadata}
+#'   ...
+#' }
+"pancreasSCE"
+
+#' Example ImageList object
+#'
+#' TODO
+#'
+#' @format An ImageList object containing ...
+#' \describe{
+#'   \item{...}{...}
+#'   \item{...}{...}
+#'   ...
+#' }
+"pancreasImage"
+
