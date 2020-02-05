4cdc8c07ffae7b5008af631647e614c71356167e
nilseling <nils.eling@gmx.de>
2020-02-05 10:52:56 +0100

    Docs on images and masks

--- R/data.R
+++ R/data.R
@@ -47,25 +47,32 @@
 
 #' Example ImageList object of image files
 #'
-#' TODO
+#' This \linkS4class{ImageList} object contains multi-channel stacks of three images
+#' acquired by imaging mass cytometry. Each channel represents the pixel-intensities
+#' of each of the 5 measured proteins. The data is part of a imaging mass cytometry
+#' study on the progression of Type 1 diabetes and contains pancreas cells.
 #'
-#' @format An ImageList object containing ...
-#' \describe{
-#'   \item{...}{...}
-#'   \item{...}{...}
-#'   ...
-#' }
+#' @format An ImageList object containing 3 \code{\linkS4class{Image}} objects
+#' with 5 channels each. Channel names can be accessed via \code{?"\link{channelNames}"}.
+#'
+#' @references Damond, N. et al., A Map of Human Type 1 Diabetes Progression by
+#' Imaging Mass Cytometry, Cell Metabolism 29:3, 2019
 "pancreasImages"
 
 #' Example ImageList object of segmentation masks
 #'
-#' TODO
+#' This \linkS4class{ImageList} object contains single-channel images representing
+#' the segmentation masks after preprocessing of imaging mass cytometry data.
+#' The data is part of a imaging mass cytometry
+#' study on the progression of Type 1 diabetes and contains pancreas cells.
 #'
-#' @format An ImageList object containing ...
-#' \describe{
-#'   \item{...}{...}
-#'   \item{...}{...}
-#'   ...
-#' }
+#' @format An ImageList object containing 3 \code{\linkS4class{Image}} objects
+#' with 1 channel each. These images are the result to segmentation and associated
+#' to the images stored in the \code{\link{pancreasImages}} object.
+#' Pixel values indicate the numeric cell identifier while a value of 0 represents the
+#' image background.
+#'
+#' @references Damond, N. et al., A Map of Human Type 1 Diabetes Progression by
+#' Imaging Mass Cytometry, Cell Metabolism 29:3, 2019
 "pancreasMasks"
 
