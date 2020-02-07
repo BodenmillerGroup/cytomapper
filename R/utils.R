b4ee6508517931915c17b8328ed1dd4d0187f624
nilseling <nils.eling@gmx.de>
2020-02-07 12:35:47 +0100

    Draft subsetting wrappers

--- R/utils.R
+++ R/utils.R
@@ -75,9 +75,92 @@ setReplaceMethod("channelNames",
                    return(x)
                  })
 
-#setMethod("getChannels",
-#          signature = signature(x="ImageList"),
-#          definition = function(x){dimnames(x[[1]])[[3]]})
+#' @title Getting and setting individual channels and images
+#' @name ImageList-subsetting
+#'
+#' @description
+#' These getter and setter functions are used to extracting and replacing
+#' entries in an \code{\linkS4class{ImageList}} object.
+#'
+#'
+#' @details
+#' These methods are preferred over subsetting via `[<-` or `[[<-`, which by default
+#' does not replace the entry names.
+#'
+#' The `[<-` or `[[<-` functions for the \code{\linkS4class{ImageList}} object
+#' have been adjusted to call getImages and setImages.
+#'
+#' @param x TODO
+#' @param value TODO
+#'
+#' @return An ImageList object
+#'
+#' @author
+#' Nils Eling \email{nils.eling@@dqbm.uzh.ch}
+#' Nicolas Damond \email{nicolas.damond@@dqbm.uzh.ch}
+NULL
+
+#' @export
+#' @rdname ImageList-subsetting
+setMethod("getImages",
+          signature = signature(x="ImageList"),
+          definition = function(x, value){
+
+            ans.list <- as.list(x)
+            ans.mcols <- mcols(x)
+
+            # Initial checks
+            if(is.null || (!is.numeric && !is.character)){
+              stop("Invalid argument for 'value'")
+            }
+
+            if(is.numeric(value)){
+              cur_list <- ans.list[value]
+              if(is.null(ans.mcols)){
+                cur_mcols <- NULL
+              } else {
+                cur_mcols <- ans.mcols[value,]
+              }
+            }
+
+            if(is.character(value)){
+              cur_list <- ans.list[value]
+              if(is.null(ans.mcols)){
+                cur_mcols <- NULL
+              } else {
+                cur_mcols <- ans.mcols[value,]
+              }
+            }
+
+            cur_ImageList <- ImageList(cur_list,
+                                       elementMetadata = cur_mcols,
+                                       channelNames = channelNames(x))
+            return(cur_ImageList)
+          })
+
+#' @export
+#' @rdname ImageList-subsetting
+setReplaceMethod("setImages",
+          signature = signature(x="ImageList"),
+          definition = function(x, value){
+
+          })
+
+#' @export
+#' @rdname ImageList-subsetting
+setMethod("getChannels",
+          signature = signature(x="ImageList"),
+          definition = function(x, value){
+
+          })
+
+#' @export
+#' @rdname ImageList-subsetting
+setReplaceMethod("setChannels",
+          signature = signature(x="ImageList"),
+          definition = function(x, value){
+
+          })
 
 
 
