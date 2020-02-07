e30d4e4f985cffa97b504062db6537a2ae736e15
nilseling <nils.eling@gmx.de>
2020-02-07 12:36:08 +0100

    Generics for subsetter

--- R/AllGenerics.R
+++ R/AllGenerics.R
@@ -11,6 +11,26 @@ setGeneric("channelNames", function(x) standardGeneric("channelNames") )
 setGeneric("channelNames<-",
            function(x, value) standardGeneric("channelNames<-"))
 
+#' @export
+#' @rdname ImageList-subsetting
+setGeneric("getImages",
+           function(x, value) standardGeneric("getImages"))
+
+#' @export
+#' @rdname ImageList-subsetting
+setGeneric("setImages<-",
+           function(x, value) standardGeneric("setImages"))
+
+#' @export
+#' @rdname ImageList-subsetting
+setGeneric("getChannels",
+           function(x, value) standardGeneric("getChannels"))
+
+#' @export
+#' @rdname ImageList-subsetting
+setGeneric("setChannels<-",
+           function(x, value) standardGeneric("setChannels"))
+
 
 
 
