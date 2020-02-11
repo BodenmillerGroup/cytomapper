b15c50d9b904fe5c85c71ce25194a2af12d8b401
nilseling <nils.eling@gmx.de>
2020-02-11 14:14:47 +0100

    Extended validity checks

--- R/ImageList-class.R
+++ R/ImageList-class.R
@@ -11,22 +11,25 @@ setClass(
   contains = "SimpleList",
   prototype = prototype(
     int_metadata=list(
-      version=utils::packageVersion("SingleCellMapper"),
-      channelNames=""),
+      version=utils::packageVersion("SingleCellMapper")),
     elementType="Image"
     ),
   )
 
 # Validity checks
-S4Vectors:::setValidity2(Class="ImageList",
-            method=function(object) {
+#' @importFrom S4Vectors setValidity2
+S4Vectors:::setValidity2(Class="ImageList", .ImageList_validity)
+
+.ImageList_validity <- function(object) {
+
+              msg <- NULL
 
               # Check if all images have the same number of channels
               dims <- unlist(lapply(object, function(x){
                 dim(x)[3]
               }))
               if(length(unique(dims)) > 1){
-                stop("The images contain different number of channels.\n")
+                msg <- c(msg, "The images contain different number of channels.\n")
               }
 
               # Check if all channels have the same names
@@ -35,29 +38,30 @@ S4Vectors:::setValidity2(Class="ImageList",
                 !identical(cur_names, dimnames(x)[[3]])
               }))
               if(sum(errors) > 0){
-                stop("Not all channels have the same names.\n",
-                     "Please use the 'channelNames' function to alter channel names.")
+                msg <- c(msg, "Not all channels have the same names.")
               }
 
               # Check if entry names are unique
               if(!is.null(names(object)) &&
                  length(unique(names(object))) < length(names(object))){
-                stop("Only unique entries allowed in an ImageList object.")
+                msg <- c(msg, "Only unique entries allowed in an ImageList object.")
               }
 
               # Check if names contain NA or empties
               if(!is.null(names(object)) && (sum(is.na(names(object))) > 0 ||
                                              sum(names(object) %in% "") > 0)){
-                stop("Empty or NA names not supported.")
+                msg <- c(msg, "Empty or NA names not supported.")
               }
 
               # Check if colourmode of each Image is "Grayscale"
               colour.modes <- unlist(lapply(object, colorMode))
               if("Color" %in% colour.modes){
-                stop("Only Grayscale images are supported for ImageList objects.")
+                msg <- c(msg, "Only Grayscale images are supported for ImageList objects.")
               }
+
+              if (length(msg)) { return(msg) }
+              return(TRUE)
             }
-)
 
 # Coercion from list
 setAs("list", "ImageList", function(from) {
@@ -78,7 +82,33 @@ setAs("ANY", "ImageList", function(from) {
 
 #setMethod("plot")
 
-#setMethod("names")
+# Expand bracket functions to check if valid object is returned
+setReplaceMethod("[",
+                 signature = signature(x="ImageList"),
+                 definition = function(x, i, j, ..., value){
+                   .Object <- callNextMethod()
+                   .Object <- as(.Object, "ImageList")
+                   methods::validObject(.Object)
+                   return(.Object)
+                 })
+
+setReplaceMethod("[[",
+                 signature = signature(x="ImageList"),
+                 definition = function(x, i, j, ..., value){
+                 .Object <- callNextMethod()
+                 .Object <- as(.Object, "ImageList")
+                 methods::validObject(.Object)
+                 return(.Object)
+                 })
+
+setReplaceMethod("names",
+                 signature = signature(x="ImageList"),
+                 definition = function(x, value){
+                 .Object <- callNextMethod()
+                 .Object <- as(.Object, "ImageList")
+                 methods::validObject(.Object)
+                 return(.Object)
+                 })
 
 
 
