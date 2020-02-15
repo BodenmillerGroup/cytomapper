0aa0a33f8d52995614690bb33a99ce1db338f4a3
nilseling <nils.eling@gmx.de>
2020-02-15 10:27:56 +0100

    Further docs and examples on subsetting

--- R/subsetting-utils.R
+++ R/subsetting-utils.R
@@ -2,41 +2,136 @@
 # Utility functions for ImageList subsetting
 # -------------------------------------------------------
 
-#' @title Getting and setting individual channels and images
+#' @title General subsetting methods for ImageList objects
 #' @name ImageList-subsetting
 #'
 #' @description
-#' These getter and setter functions are used to extracting and replacing
+#' These getter and setter functions are used to extract, replace and merge
 #' entries in an \code{\linkS4class{ImageList}} object.
 #'
+#' @param x,y \code{ImageList} objects
+#' @param i integer, logical, character or vector of such indicating which
+#'   element(s) to replace or extract
+#' @param value an \code{ImageList} or \code{\linkS4class{Image}}
+#' object
 #'
-#' @details
-#' These methods are preferred over subsetting via `[<-` or `[[<-`, which by default
-#' does not replace the entry names.
+#' @section Setting and getting images:
+#' Functions to extract and replace elements (= images) of an
+#' \linkS4class{ImageList} object. In the following code, \code{x} is an
+#' ImageList object. The parameter \code{i} indicates the
+#' element(s) of \code{x} that should be returned or replaced. Replacement is
+#' done by \code{value}, which takes an ImageList or Image object. If
+#' \code{length(i) > 0}, \code{value} has to be an ImageList object of
+#' \code{length(i)}, otherwise \code{value} allows an ImageList object of length
+#' 1 or an Image object.
 #'
-#' It is only allowed to replace explicitily named entried in named ImageList
+#' \describe{
+#' \item{\code{getImages(x, i)}:}{Returns image(s) indicated by \code{i} of
+#' the ImageList object \code{x}}
+#' \item{\code{setImages(x, i) <- value}:}{Replaces the image(s) indicated by
+#' \code{i} of the ImageList object \code{x} with \code{value}. For this,
+#' \code{value} needs to have the same length as \code{i}}
+#' }
 #'
-#' @section Getting and setting images:
-#' TODO
+#' These setter and getter functions are the recommended way of extracting and replacing
+#' images in an ImageList object. Alternatively, the standard operations via `[`, `[[`,
+#' `[<-` and `[[<-` can be performed (see \code{?\link{List}} for S4Vectors subsetting
+#' functionality). However, these operations do not change element names during replacment
+#' calls. The \code{setImages()} function makes sure that element names are replaced if
+#' \code{value} is named or of \code{i} is a character or vector of characters.
 #'
 #' @section Getting and setting channels:
-#' TODO
-#' Discuss adding channels to list
+#' Functions to extract and replace channels of an \linkS4class{ImageList}
+#' object. Here, \code{x} is an \linkS4class{ImageList} object. The parameter
+#' \code{i} indicates the channels of \code{x} that should be returned or
+#' replaced. Replacement is done by \code{value}, which takes an ImageList object.
+#' The ImageList object \code{value} needs to have the same length as \code{x}.
+#' Furthermore, the number of channels in \code{value} should be identical to
+#' \code{length(i)}.
 #'
-#' @param x TODO
-#' @param i,j TODO
-#' @param .. TODO
-#' @param value TODO
+#' \describe{
+#' \item{\code{getChannels(x, i)}:}{Returns channel(s) indicated by \code{i} of
+#' the ImageList object \code{x}}
+#' \item{\code{setChannels(x, i) <- value}:}{Replaces the channel(s) indicated by
+#' \code{i} of the ImageList object \code{x} with \code{value}. For this,
+#' \code{value} needs to have the same length as \code{i} and the same
+#' number of channels as \code{length(i)}.}
+#' }
+#'
+#' The \code{setChannels()} setter function does not allow adding new channels to
+#' the ImageList object. For this operation, the \code{mergeChannels} function
+#' was implemented (see below).
+#'
+#' @section Merging images:
+#' Merging images is possible by merging two or more ImageList objects via:
+#'
+#' \describe{
+#' \item{\code{c(x,y)}:}{Returns an composite ImageList object with
+#' elements of both ImageList objects \code{x} and \code{y}.
+#' More than two ImageList objects can be merged in that way.}
+#' }
+#'
+#' @section Merging channels:
+#' Merging channels is possible via:
+#'
+#' \describe{
+#' \item{\code{mergeChannels(x,y)}:}{Returns an ImageList in which
+#' the channels of the ImageList object \code{y} have been appended
+#' to the channels of the ImageList object \code{x}.
+#' Only channels of two ImageList objects can be merged in that way.}
+#' }
 #'
 #' @return An ImageList object
 #'
+#' @examples
+#' data("pancreasImages")
+#'
+#' # Get images
+#' getImages(pancreasImages, 1)
+#' getImages(pancreasImages, "A02")
+#' getImages(pancreasImages, 1:2)
+#' getImages(pancreasImages, c("A02", "D01"))
+#' getImages(pancreasImages, grepl("A02", names(pancreasImages)))
+#'
+#' # Set images
+#' setImages(pancreasImages, 1) <- pancreasImages[1]
+#' setImages(pancreasImages, "F02") <- pancreasImages[1]
+#' setImages(pancreasImages, "F02") <- NULL
+#'
+#' # Get channels
+#' getChannels(pancreasImages, 1)
+#' getChannels(pancreasImages, "SMA")
+#' getChannels(pancreasImages, c("SMA", "INS"))
+#'
+#' # Set channels
+#' channel1 <- getChannels(pancreasImages, 1)
+#' setChannels(pancreasImages, 1) <- channel1
+#' channelINS <- getChannels(pancreasImages, "INS")
+#' setChannels(pancreasImages, "CD38") <- channelINS
+#' setChannels(pancreasImages, "CD38") <- NULL
+#'
+#' # Merge images
+#' c(pancreasImages[c(1,3)], pancreasImages[2])
+#'
+#' # Merge channels
+#' channel12 <- getChannels(pancreasImages, c(1,2))
+#' channel34 <- getChannels(pancreasImages, c(3,4))
+#' mergeChannels(channel12, channel34)
+#'
+#' @aliases
+#' getImages setImages getChannels setChannels mergeChannels
+#' getImages,ImageList-method
+#' setImages<-,ImageList-method
+#' getChannels,ImageList-method
+#' setChannels<-,ImageList-method
+#'
+#' @docType methods
+#'
 #' @author
 #' Nils Eling \email{nils.eling@@dqbm.uzh.ch}
-#' Nicolas Damond \email{nicolas.damond@@dqbm.uzh.ch}
 NULL
 
 #' @export
-#' @rdname ImageList-subsetting
 setMethod("getImages",
           signature = signature(x="ImageList"),
           definition = function(x, i){
@@ -57,7 +152,6 @@ setMethod("getImages",
           })
 
 #' @export
-#' @rdname ImageList-subsetting
 setReplaceMethod("setImages",
                  signature = signature(x="ImageList"),
                  definition = function(x, i, value){
@@ -99,7 +193,6 @@ setReplaceMethod("setImages",
                  })
 
 #' @export
-#' @rdname ImageList-subsetting
 #' @importFrom S4Vectors endoapply
 #' @importFrom methods validObject
 setMethod("getChannels",
@@ -128,7 +221,6 @@ setMethod("getChannels",
           })
 
 #' @export
-#' @rdname ImageList-subsetting
 #' @importFrom S4Vectors mendoapply
 #' @importFrom methods is validObject
 setReplaceMethod("setChannels",
@@ -180,7 +272,6 @@ setReplaceMethod("setChannels",
                  })
 
 # Expand bracket functions to check if valid object is returned
-#' @rdname ImageList-subsetting
 #' @export
 #' @importFrom methods callNextMethod as validObject
 setReplaceMethod("[",
@@ -192,7 +283,6 @@ setReplaceMethod("[",
                    return(.Object)
                  })
 
-#' @rdname ImageList-subsetting
 #' @export
 #' @importFrom methods callNextMethod as validObject
 setReplaceMethod("[[",
@@ -204,21 +294,7 @@ setReplaceMethod("[[",
                    return(.Object)
                  })
 
-#' @title Merging channels of two ImageList objects
-#' @name mergeChannels
-#' @description TODO
-#'
-#' @details
-#' # TODO
-#'
-#' @param x TODO
-#' @param y TODO
-#'
-#' @author
-#' Nils Eling \email{nils.eling@@dqbm.uzh.ch}
-#' Nicolas Damond \email{nicolas.damond@@dqbm.uzh.ch}
 #' @export
-#'
 #' @importFrom S4Vectors mendoapply
 #' @importFrom methods is validObject
 #' @importFrom EBImage abind
---
+++ tests/testthat/test_ImageList-subsetting.R
@@ -0,0 +1,207 @@
+test_that("Coercion works on ImageList object.", {
+  data("pancreasImages")
+
+  expect_silent(test.list1 <- as.list(pancreasImages))
+
+  expect_silent(test.list2 <- as(pancreasImages, "List"))
+  expect_identical(mcols(test.list2), mcols(pancreasImages))
+  expect_identical(test.list2[[1]], pancreasImages[[1]])
+
+  expect_silent(test.list3 <- as(pancreasImages, "SimpleList"))
+  expect_identical(mcols(test.list3), mcols(pancreasImages))
+  expect_identical(test.list3[[1]], pancreasImages[[1]])
+
+  expect_silent(test.list <- as(test.list1, "ImageList"))
+  expect_true(is.null(mcols(test.list)))
+
+  expect_silent(test.list <- as(test.list2, "ImageList"))
+  expect_identical(mcols(test.list2), mcols(test.list))
+  expect_identical(test.list2[[1]], test.list[[1]])
+
+  expect_silent(test.list <- as(test.list3, "ImageList"))
+  expect_identical(mcols(test.list3), mcols(test.list))
+  expect_identical(test.list3[[1]], test.list[[1]])
+})
+
+test_that("Merging works on ImageList object.", {
+  data("pancreasImages")
+  # Merging
+  ## Should work
+  expect_silent(test.list <- c(pancreasImages[c(1,3)], pancreasImages[2]))
+  expect_s4_class(test.list, "ImageList")
+  expect_equal(names(test.list), c("A02", "F01", "D01"))
+  expect_equal(rownames(mcols(test.list)), c("A02", "F01", "D01"))
+  expect_identical(test.list[[3]], pancreasImages[[2]])
+
+  ## Should fail due to duplicated names
+  expect_error(test.list <- c(pancreasImages, pancreasImages))
+
+  # Merge channels
+})
+
+test_that("General operations work on ImageList object.", {
+  # Subsetting
+  ## Getters
+  expect_true(is(pancreasImages[1], "ImageList"))
+  expect_true(is(pancreasImages[[1]], "Image"))
+
+  expect_equal(names(pancreasImages), c("A02", "D01", "F01"))
+  expect_equal(rownames(mcols(pancreasImages)), c("A02", "D01", "F01"))
+
+  ## Setters
+  #### These checks are mainly in place to avoid NA or empty names
+  cur_Images <- pancreasImages
+  names(cur_Images) <- NULL
+
+  ### Should fail
+  expect_error(cur_Images["test"] <- pancreasImages[1])
+  expect_error(names(cur_Images) <- c("test1", "test2"))
+  expect_error(cur_Images[1] <- pancreasImages[[1]])
+
+  ### Should work
+  expect_silent(cur_Images[1] <- pancreasImages[1])
+  expect_silent(cur_Images[1] <- as(pancreasImages[[2]], "ImageList"))
+
+  names(cur_Images) <- c("test1", "test2", "test3")
+  expect_equal(names(cur_Images), c("test1", "test2", "test3"))
+  expect_equal(rownames(mcols(cur_Images)), c("test1", "test2", "test3"))
+
+  expect_error(cur_Images[1] <- "test")
+  expect_error(cur_Images[[1]] <- "test")
+
+  ### Test mcols
+  cur_Images1 <- pancreasImages
+  cur_Images2 <- pancreasImages
+  names(cur_Images2) <- c("test1", "test2", "test3")
+  mcols(cur_Images2)$ImageNumber <- mcols(cur_Images2)$ImageNb
+
+  cur_Images3 <- c(cur_Images1, cur_Images2)
+  expect_true(is.na(mcols(cur_Images3[1,2])))
+
+  ### Test channel subsetting
+  cur_Images <- pancreasImages
+  expect_error(cur_Images[1] <- as(cur_Images[[1]][,,1], "ImageList"))
+
+  ## Looping
+  ### Should work
+  expect_silent(cur_list <- lapply(pancreasImages, dim))
+  expect_true(is.list(cur_list))
+  expect_silent(cur_list <- endoapply(pancreasImages, function(x){
+    channelNames(x) <- NULL
+    return(x)}))
+  expect_true(is(cur_list, "ImageList"))
+
+  ### Should not work
+  expect_error(cur_list <- endoapply(pancreasImages, dim))
+
+  ## Vector functions
+  expect_equal(length(pancreasImages), 3L)
+  expect_null(dim(pancreasImages))
+})
+
+test_that("Custom accessors work on ImageList object.", {
+  # Accessors
+  ## getImages
+  ### Should work
+  expect_s4_class(getImages(pancreasImages, "A02"), "ImageList")
+  expect_s4_class(getImages(pancreasImages, "A02")[[1]], "Image")
+  expect_s4_class(getImages(pancreasImages, 1), "ImageList")
+  expect_s4_class(getImages(pancreasImages, 1)[[1]], "Image")
+  expect_s4_class(getImages(pancreasImages, c(TRUE, FALSE, FALSE)), "ImageList")
+  expect_s4_class(getImages(pancreasImages, c(TRUE, FALSE, FALSE))[[1]], "Image")
+  expect_s4_class(getImages(pancreasImages, c("A02", "D01")), "ImageList")
+  expect_s4_class(getImages(pancreasImages, c(1,2)), "ImageList")
+
+  ### Should not work
+  expect_error(getImages(pancreasImages, "A"))
+  expect_error(getImages(pancreasImages, "A02", "test"))
+  expect_error(getImages(pancreasImages, c("A02", "test")))
+  expect_error(getImages(pancreasImages, 4))
+
+  ## setImages
+  cur_Images1 <- pancreasImages
+  cur_Images2 <- pancreasImages
+  names(cur_Images2) <- c("test1", "test2", "test3")
+  mcols(cur_Images2)$ImageNumber <- mcols(cur_Images2)$ImageNb
+  cur_Images3 <- cur_Images2
+  names(cur_Images3) <- NULL
+
+  ### Should work
+  expect_silent(setImages(cur_Images1, "A02") <- cur_Images2[1])
+  expect_equal(names(cur_Images1), c("A02", "D01", "F01"))
+  expect_equal(channelNames(cur_Images1), c("H3", "SMA", "INS", "CD38", "CD44"))
+  expect_silent(setImages(cur_Images1, "A02") <- cur_Images2[[1]])
+  expect_silent(setImages(cur_Images1, "G02") <- cur_Images2[1])
+  expect_equal(names(cur_Images1), c("A02", "D01", "F01", "G02"))
+  expect_equal(channelNames(cur_Images1), c("H3", "SMA", "INS", "CD38", "CD44"))
+  expect_silent(setImages(cur_Images1, 1) <- cur_Images2[2])
+  expect_equal(names(cur_Images1), c("test2", "D01", "F01", "G02"))
+  expect_equal(channelNames(cur_Images1), c("H3", "SMA", "INS", "CD38", "CD44"))
+  expect_silent(setImages(cur_Images1, 1:2) <- cur_Images2[2:3])
+  expect_equal(names(cur_Images1), c("test2", "test3", "F01", "G02"))
+  expect_equal(channelNames(cur_Images1), c("H3", "SMA", "INS", "CD38", "CD44"))
+  expect_silent(setImages(cur_Images3, 1) <- cur_Images3[2])
+  expect_equal(channelNames(cur_Images3), c("H3", "SMA", "INS", "CD38", "CD44"))
+
+  ### Should not work
+  expect_error(setImages(cur_Images1, 1) <- cur_Images2[[1]])
+  expect_error(setImages(cur_Images1, 1:2) <- cur_Images2[2])
+  expect_error(setImages(cur_Images1, 1) <- cur_Images3[2])
+  expect_error(setImages(cur_Images3, 1) <- pancreasImages[2])
+  expect_error(setImages(cur_Images3, "test") <- cur_Images3[2])
+
+  ## getChannels
+  # Build check that only unique channels are allowed
+  ### Should work
+  expect_s4_class(getChannels(pancreasImages, 1), "ImageList")
+  expect_s4_class(getChannels(pancreasImages, 1:2), "ImageList")
+  expect_s4_class(getChannels(pancreasImages, "H3"), "ImageList")
+
+  expect_silent(test <- getChannels(pancreasImages, 1))
+  expect_equal(channelNames(test), "H3")
+  expect_equal(length(test), 3L)
+  expect_equal(names(test), c("A02", "D01", "F01"))
+
+  expect_silent(test <- getChannels(pancreasImages, "H3"))
+  expect_equal(channelNames(test), "H3")
+  expect_equal(length(test), 3L)
+  expect_equal(names(test), c("A02", "D01", "F01"))
+
+  ### Should not work
+  expect_error(getChannels(pancreasImages, 10))
+  expect_error(getChannels(pancreasImages, "test"))
+  expect_error(getChannels(pancreasImages, c("H3", "test")))
+
+  ## setChannels
+  cur_Images1 <- pancreasImages
+  cur_Images2 <- getChannels(pancreasImages, 2)
+
+  ### Should work
+  expect_silent(setChannels(cur_Images1, 1) <- cur_Images2)
+  expect_equal(channelNames(cur_Images1), c("SMA", "SMA", "INS", "CD38", "CD44"))
+  expect_equal(cur_Images1[[1]][,,1], cur_Images1[[1]][,,2])
+
+  expect_silent(setChannels(cur_Images1, "INS") <- cur_Images2)
+  expect_equal(channelNames(cur_Images1), c("SMA", "SMA", "INS", "CD38", "CD44"))
+  expect_equal(cur_Images1[[1]][,,2], cur_Images1[[1]][,,3])
+
+  cur_Images1 <- pancreasImages
+  cur_Images2 <- getChannels(pancreasImages, 2:3)
+
+  expect_silent(setChannels(cur_Images1, 1:2) <- cur_Images2)
+  expect_equal(channelNames(cur_Images1), c("SMA", "INS", "INS", "CD38", "CD44"))
+  expect_equal(cur_Images1[[1]][,,2], cur_Images1[[1]][,,3])
+
+  expect_silent(setChannels(cur_Images1, 1:2) <- NULL)
+  expect_equal(channelNames(cur_Images1), c("INS", "CD38", "CD44"))
+
+  # Check that setting NULL works
+
+  ### Should not work
+  cur_Images1 <- pancreasImages
+  cur_Images2 <- getChannels(pancreasImages, 2)
+  expect_error(setChannels(cur_Images1, 6) <- cur_Images2)
+  expect_error(setChannels(cur_Images1, "test") <- cur_Images2)
+  expect_error(setChannels(cur_Images1, 1) <- "test")
+})
+
