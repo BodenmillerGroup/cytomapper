120b21671814179d7d3536e5608dbd5e7cc8a5be
nilseling <nils.eling@gmx.de>
2020-03-01 18:46:45 +0100

    Script on further parameters

---
+++ R/plotting-param.R
@@ -0,0 +1,26 @@
+#' Further plotting parameters for the SingleCellMapper package
+#' @name plotting-param
+#'
+#' TODO
+#'
+#' @param legend_size TODO
+#' @param title_size TODO
+#' @param title_colour TODO
+#' @param return_plot TODO
+#' @param return_images TODO
+#'
+#'@section Setting further parameters:
+#' TODO
+#'
+#' @return TODO
+#'
+#' @examples
+#' TODO
+#'
+#' @author Nils Eling (\email{nils.eling@@dqbm.uzh.ch})
+#' @author Nicolas Damond (\email{nicolas.damond@@dqbm.uzh.ch})
+#'
+#' @export
+NULL
+
+# Plotting parameters checks
