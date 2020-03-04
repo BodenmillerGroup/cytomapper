c7bf653234f3f5b9e06768ee2bf0749103aa2e6b
nilseling <nils.eling@gmx.de>
2020-03-04 17:39:30 +0100

    Change plotting parameters

--- R/plotting-param.R
+++ R/plotting-param.R
@@ -4,9 +4,8 @@
 #' @description
 #' TODO
 #'
-#' @param legend_size TODO
-#' @param title_size TODO
-#' @param title_colour TODO
+#' @param legend list(title, size) also allow NULL
+#' @param image_title list(label, size, col)
 #' @param return_plot TODO
 #' @param return_images TODO
 #' @param save_images TODO
