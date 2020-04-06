5fdd4146f17d9edafe88b6310ff48af77e05b2b4
nilseling <nils.eling@gmx.de>
2020-04-06 15:17:41 +0200

    Added .colourMaskByFeature unit tests

--- tests/testthat/test_plotting-utils.R
+++ tests/testthat/test_plotting-utils.R
@@ -136,7 +136,248 @@ test_that("masks can be coloured by metadata.", {
 })
 
 test_that("masks can be coloured by features.", {
-  # .colourMaskByFeature
+    data("pancreasMasks")
+    data("pancreasSCE")
+    
+    cur_col <- list(H3 = c("black", "red"), SMA = c("black", "green"), CD44 = c("black", "blue"))
+    plottingParam <- list(scale = TRUE)
+    
+    expect_silent(cur_out <- .colourMaskByFeature(object = pancreasSCE[,pancreasSCE$ImageNb == 1], mask = pancreasMasks[1], 
+                                                  cell_id = "CellNb", img_id = "ImageNb",
+                                                  colour_by = c("H3", "SMA", "CD44"), 
+                                                  exprs_values = "counts", cur_colour = cur_col,
+                                                  missing_colour = "grey", background_colour = "black", 
+                                                  plottingParam = plottingParam))
+    
+    # Check if overlayed image is the same as EBImage output
+    cur_img <- combine(pancreasMasks[[1]], pancreasMasks[[1]], pancreasMasks[[1]])
+    cur_sce <- pancreasSCE[,pancreasSCE$ImageNb == 1]
+    cur_img_H3 <- pancreasMasks[[1]]
+    cur_img_H3[cur_img_H3 == 0L] <- NA
+    cur_ind <- match(cur_img_H3, cur_sce$CellNb)
+    cur_img_H3[!is.na(cur_ind)] <- assay(cur_sce, "counts")["H3",cur_ind[!is.na(cur_ind)]]
+    cur_img[,,1] <- cur_img_H3
+    cur_img_SMA <- pancreasMasks[[1]]
+    cur_img_SMA[cur_img_SMA == 0L] <- NA
+    cur_ind <- match(cur_img_SMA, cur_sce$CellNb)
+    cur_img_SMA[!is.na(cur_ind)] <- assay(cur_sce, "counts")["SMA",cur_ind[!is.na(cur_ind)]]
+    cur_img[,,2] <- cur_img_SMA
+    cur_img_CD44 <- pancreasMasks[[1]]
+    cur_img_CD44[cur_img_CD44 == 0L] <- NA
+    cur_ind <- match(cur_img_CD44, cur_sce$CellNb)
+    cur_img_CD44[!is.na(cur_ind)] <- assay(cur_sce, "counts")["CD44",cur_ind[!is.na(cur_ind)]]
+    cur_img[,,3] <- cur_img_CD44
+    
+    cur_img <- EBImage::normalize(cur_img, separate = TRUE)
+    cur_img[is.na(cur_img)] <- 0
+    
+    expect_equal(Image(cur_out[[1]]), Image(cur_img, colormode = "Color"), tolerance = 0.01)
+    
+    # Now with global scaling
+    expect_silent(cur_out <- .colourMaskByFeature(object = pancreasSCE, mask = pancreasMasks, 
+                                                  cell_id = "CellNb", img_id = "ImageNb",
+                                                  colour_by = c("H3", "SMA", "CD44"), 
+                                                  exprs_values = "counts", cur_colour = cur_col,
+                                                  missing_colour = "grey", background_colour = "black", 
+                                                  plottingParam = plottingParam))
+    
+    # Check if overlayed image is the same as EBImage output
+    cur_img <- combine(pancreasMasks[[1]], pancreasMasks[[1]], pancreasMasks[[1]])
+    cur_sce <- pancreasSCE[,pancreasSCE$ImageNb == 1]
+    cur_img_H3 <- pancreasMasks[[1]]
+    cur_img_H3[cur_img_H3 == 0L] <- NA
+    cur_ind <- match(cur_img_H3, cur_sce$CellNb)
+    cur_img_H3[!is.na(cur_ind)] <- assay(cur_sce, "counts")["H3",cur_ind[!is.na(cur_ind)]]
+    cur_img_H3 <- EBImage::normalize(cur_img_H3, separate = TRUE, 
+                                     inputRange = c(min(assay(pancreasSCE, "counts")["H3",]),
+                                                    max(assay(pancreasSCE, "counts")["H3",])))
+    cur_img[,,1] <- cur_img_H3
+    cur_img_SMA <- pancreasMasks[[1]]
+    cur_img_SMA[cur_img_SMA == 0L] <- NA
+    cur_ind <- match(cur_img_SMA, cur_sce$CellNb)
+    cur_img_SMA[!is.na(cur_ind)] <- assay(cur_sce, "counts")["SMA",cur_ind[!is.na(cur_ind)]]
+    cur_img_SMA <- EBImage::normalize(cur_img_SMA, separate = TRUE, 
+                                     inputRange = c(min(assay(pancreasSCE, "counts")["SMA",]),
+                                                    max(assay(pancreasSCE, "counts")["SMA",])))
+    cur_img[,,2] <- cur_img_SMA
+    cur_img_CD44 <- pancreasMasks[[1]]
+    cur_img_CD44[cur_img_CD44 == 0L] <- NA
+    cur_ind <- match(cur_img_CD44, cur_sce$CellNb)
+    cur_img_CD44[!is.na(cur_ind)] <- assay(cur_sce, "counts")["CD44",cur_ind[!is.na(cur_ind)]]
+    cur_img_CD44 <- EBImage::normalize(cur_img_CD44, separate = TRUE, 
+                                      inputRange = c(min(assay(pancreasSCE, "counts")["CD44",]),
+                                                     max(assay(pancreasSCE, "counts")["CD44",])))
+    cur_img[,,3] <- cur_img_CD44
+    
+    cur_img[is.na(cur_img)] <- 0
+    
+    expect_equal(Image(cur_out[[1]]), Image(cur_img, colormode = "Color"), tolerance = 0.02)
+    
+    cur_img <- combine(pancreasMasks[[2]], pancreasMasks[[2]], pancreasMasks[[2]])
+    cur_sce <- pancreasSCE[,pancreasSCE$ImageNb == 2]
+    cur_img_H3 <- pancreasMasks[[2]]
+    cur_img_H3[cur_img_H3 == 0L] <- NA
+    cur_ind <- match(cur_img_H3, cur_sce$CellNb)
+    cur_img_H3[!is.na(cur_ind)] <- assay(cur_sce, "counts")["H3",cur_ind[!is.na(cur_ind)]]
+    cur_img_H3 <- EBImage::normalize(cur_img_H3, separate = TRUE, 
+                                     inputRange = c(min(assay(pancreasSCE, "counts")["H3",]),
+                                                    max(assay(pancreasSCE, "counts")["H3",])))
+    cur_img[,,1] <- cur_img_H3
+    cur_img_SMA <- pancreasMasks[[2]]
+    cur_img_SMA[cur_img_SMA == 0L] <- NA
+    cur_ind <- match(cur_img_SMA, cur_sce$CellNb)
+    cur_img_SMA[!is.na(cur_ind)] <- assay(cur_sce, "counts")["SMA",cur_ind[!is.na(cur_ind)]]
+    cur_img_SMA <- EBImage::normalize(cur_img_SMA, separate = TRUE, 
+                                      inputRange = c(min(assay(pancreasSCE, "counts")["SMA",]),
+                                                     max(assay(pancreasSCE, "counts")["SMA",])))
+    cur_img[,,2] <- cur_img_SMA
+    cur_img_CD44 <- pancreasMasks[[2]]
+    cur_img_CD44[cur_img_CD44 == 0L] <- NA
+    cur_ind <- match(cur_img_CD44, cur_sce$CellNb)
+    cur_img_CD44[!is.na(cur_ind)] <- assay(cur_sce, "counts")["CD44",cur_ind[!is.na(cur_ind)]]
+    cur_img_CD44 <- EBImage::normalize(cur_img_CD44, separate = TRUE, 
+                                       inputRange = c(min(assay(pancreasSCE, "counts")["CD44",]),
+                                                      max(assay(pancreasSCE, "counts")["CD44",])))
+    cur_img[,,3] <- cur_img_CD44
+    
+    cur_img[is.na(cur_img)] <- 0
+    
+    expect_equal(Image(cur_out[[2]]), Image(cur_img, colormode = "Color"), tolerance = 0.025)
+    
+    cur_img <- combine(pancreasMasks[[3]], pancreasMasks[[3]], pancreasMasks[[2]])
+    cur_sce <- pancreasSCE[,pancreasSCE$ImageNb == 3]
+    cur_img_H3 <- pancreasMasks[[3]]
+    cur_img_H3[cur_img_H3 == 0L] <- NA
+    cur_ind <- match(cur_img_H3, cur_sce$CellNb)
+    cur_img_H3[!is.na(cur_ind)] <- assay(cur_sce, "counts")["H3",cur_ind[!is.na(cur_ind)]]
+    cur_img_H3 <- EBImage::normalize(cur_img_H3, separate = TRUE, 
+                                     inputRange = c(min(assay(pancreasSCE, "counts")["H3",]),
+                                                    max(assay(pancreasSCE, "counts")["H3",])))
+    cur_img[,,1] <- cur_img_H3
+    cur_img_SMA <- pancreasMasks[[3]]
+    cur_img_SMA[cur_img_SMA == 0L] <- NA
+    cur_ind <- match(cur_img_SMA, cur_sce$CellNb)
+    cur_img_SMA[!is.na(cur_ind)] <- assay(cur_sce, "counts")["SMA",cur_ind[!is.na(cur_ind)]]
+    cur_img_SMA <- EBImage::normalize(cur_img_SMA, separate = TRUE, 
+                                      inputRange = c(min(assay(pancreasSCE, "counts")["SMA",]),
+                                                     max(assay(pancreasSCE, "counts")["SMA",])))
+    cur_img[,,2] <- cur_img_SMA
+    cur_img_CD44 <- pancreasMasks[[3]]
+    cur_img_CD44[cur_img_CD44 == 0L] <- NA
+    cur_ind <- match(cur_img_CD44, cur_sce$CellNb)
+    cur_img_CD44[!is.na(cur_ind)] <- assay(cur_sce, "counts")["CD44",cur_ind[!is.na(cur_ind)]]
+    cur_img_CD44 <- EBImage::normalize(cur_img_CD44, separate = TRUE, 
+                                       inputRange = c(min(assay(pancreasSCE, "counts")["CD44",]),
+                                                      max(assay(pancreasSCE, "counts")["CD44",])))
+    cur_img[,,3] <- cur_img_CD44
+    
+    cur_img[is.na(cur_img)] <- 0
+    
+    expect_equal(Image(cur_out[[3]]), Image(cur_img, colormode = "Color"), tolerance = 0.025)
+    
+    # Set scaling to FALSE
+    plottingParam <- list(scale = FALSE)
+    colour_by <- c("H3", "SMA", "CD44")
+    expect_silent(cur_out <- .colourMaskByFeature(object = pancreasSCE, mask = pancreasMasks, 
+                                                  cell_id = "CellNb", img_id = "ImageNb",
+                                                  colour_by = colour_by, 
+                                                  exprs_values = "counts", cur_colour = cur_col,
+                                                  missing_colour = "grey", background_colour = "black", 
+                                                  plottingParam = plottingParam))
+    
+    # Check if overlayed image is the same as EBImage output
+    cur_img <- combine(pancreasMasks[[1]], pancreasMasks[[1]], pancreasMasks[[1]])
+    cur_sce <- pancreasSCE[,pancreasSCE$ImageNb == 1]
+    cur_img_H3 <- pancreasMasks[[1]]
+    cur_img_H3[cur_img_H3 == 0L] <- NA
+    cur_ind <- match(cur_img_H3, cur_sce$CellNb)
+    cur_img_H3[!is.na(cur_ind)] <- assay(cur_sce, "counts")["H3",cur_ind[!is.na(cur_ind)]]
+    cur_img_H3 <- EBImage::normalize(cur_img_H3, separate = TRUE, 
+                                     inputRange = c(min(assay(pancreasSCE, "counts")[colour_by,]),
+                                                    max(assay(pancreasSCE, "counts")[colour_by,])))
+    cur_img[,,1] <- cur_img_H3
+    cur_img_SMA <- pancreasMasks[[1]]
+    cur_img_SMA[cur_img_SMA == 0L] <- NA
+    cur_ind <- match(cur_img_SMA, cur_sce$CellNb)
+    cur_img_SMA[!is.na(cur_ind)] <- assay(cur_sce, "counts")["SMA",cur_ind[!is.na(cur_ind)]]
+    cur_img_SMA <- EBImage::normalize(cur_img_SMA, separate = TRUE, 
+                                      inputRange = c(min(assay(pancreasSCE, "counts")[colour_by,]),
+                                                     max(assay(pancreasSCE, "counts")[colour_by,])))
+    cur_img[,,2] <- cur_img_SMA
+    cur_img_CD44 <- pancreasMasks[[1]]
+    cur_img_CD44[cur_img_CD44 == 0L] <- NA
+    cur_ind <- match(cur_img_CD44, cur_sce$CellNb)
+    cur_img_CD44[!is.na(cur_ind)] <- assay(cur_sce, "counts")["CD44",cur_ind[!is.na(cur_ind)]]
+    cur_img_CD44 <- EBImage::normalize(cur_img_CD44, separate = TRUE, 
+                                       inputRange = c(min(assay(pancreasSCE, "counts")[colour_by,]),
+                                                      max(assay(pancreasSCE, "counts")[colour_by,])))
+    cur_img[,,3] <- cur_img_CD44
+    
+    cur_img[is.na(cur_img)] <- 0
+    
+    expect_equal(Image(cur_out[[1]]), Image(cur_img, colormode = "Color"), tolerance = 0.025)
+    
+    cur_img <- combine(pancreasMasks[[2]], pancreasMasks[[2]], pancreasMasks[[2]])
+    cur_sce <- pancreasSCE[,pancreasSCE$ImageNb == 2]
+    cur_img_H3 <- pancreasMasks[[2]]
+    cur_img_H3[cur_img_H3 == 0L] <- NA
+    cur_ind <- match(cur_img_H3, cur_sce$CellNb)
+    cur_img_H3[!is.na(cur_ind)] <- assay(cur_sce, "counts")["H3",cur_ind[!is.na(cur_ind)]]
+    cur_img_H3 <- EBImage::normalize(cur_img_H3, separate = TRUE, 
+                                     inputRange = c(min(assay(pancreasSCE, "counts")[colour_by,]),
+                                                    max(assay(pancreasSCE, "counts")[colour_by,])))
+    cur_img[,,1] <- cur_img_H3
+    cur_img_SMA <- pancreasMasks[[2]]
+    cur_img_SMA[cur_img_SMA == 0L] <- NA
+    cur_ind <- match(cur_img_SMA, cur_sce$CellNb)
+    cur_img_SMA[!is.na(cur_ind)] <- assay(cur_sce, "counts")["SMA",cur_ind[!is.na(cur_ind)]]
+    cur_img_SMA <- EBImage::normalize(cur_img_SMA, separate = TRUE, 
+                                      inputRange = c(min(assay(pancreasSCE, "counts")[colour_by,]),
+                                                     max(assay(pancreasSCE, "counts")[colour_by,])))
+    cur_img[,,2] <- cur_img_SMA
+    cur_img_CD44 <- pancreasMasks[[2]]
+    cur_img_CD44[cur_img_CD44 == 0L] <- NA
+    cur_ind <- match(cur_img_CD44, cur_sce$CellNb)
+    cur_img_CD44[!is.na(cur_ind)] <- assay(cur_sce, "counts")["CD44",cur_ind[!is.na(cur_ind)]]
+    cur_img_CD44 <- EBImage::normalize(cur_img_CD44, separate = TRUE, 
+                                       inputRange = c(min(assay(pancreasSCE, "counts")[colour_by,]),
+                                                      max(assay(pancreasSCE, "counts")[colour_by,])))
+    cur_img[,,3] <- cur_img_CD44
+    
+    cur_img[is.na(cur_img)] <- 0
+    
+    expect_equal(Image(cur_out[[2]]), Image(cur_img, colormode = "Color"), tolerance = 0.035)
+    
+    cur_img <- combine(pancreasMasks[[3]], pancreasMasks[[3]], pancreasMasks[[2]])
+    cur_sce <- pancreasSCE[,pancreasSCE$ImageNb == 3]
+    cur_img_H3 <- pancreasMasks[[3]]
+    cur_img_H3[cur_img_H3 == 0L] <- NA
+    cur_ind <- match(cur_img_H3, cur_sce$CellNb)
+    cur_img_H3[!is.na(cur_ind)] <- assay(cur_sce, "counts")["H3",cur_ind[!is.na(cur_ind)]]
+    cur_img_H3 <- EBImage::normalize(cur_img_H3, separate = TRUE, 
+                                     inputRange = c(min(assay(pancreasSCE, "counts")[colour_by,]),
+                                                    max(assay(pancreasSCE, "counts")[colour_by,])))
+    cur_img[,,1] <- cur_img_H3
+    cur_img_SMA <- pancreasMasks[[3]]
+    cur_img_SMA[cur_img_SMA == 0L] <- NA
+    cur_ind <- match(cur_img_SMA, cur_sce$CellNb)
+    cur_img_SMA[!is.na(cur_ind)] <- assay(cur_sce, "counts")["SMA",cur_ind[!is.na(cur_ind)]]
+    cur_img_SMA <- EBImage::normalize(cur_img_SMA, separate = TRUE, 
+                                      inputRange = c(min(assay(pancreasSCE, "counts")[colour_by,]),
+                                                     max(assay(pancreasSCE, "counts")[colour_by,])))
+    cur_img[,,2] <- cur_img_SMA
+    cur_img_CD44 <- pancreasMasks[[3]]
+    cur_img_CD44[cur_img_CD44 == 0L] <- NA
+    cur_ind <- match(cur_img_CD44, cur_sce$CellNb)
+    cur_img_CD44[!is.na(cur_ind)] <- assay(cur_sce, "counts")["CD44",cur_ind[!is.na(cur_ind)]]
+    cur_img_CD44 <- EBImage::normalize(cur_img_CD44, separate = TRUE, 
+                                       inputRange = c(min(assay(pancreasSCE, "counts")[colour_by,]),
+                                                      max(assay(pancreasSCE, "counts")[colour_by,])))
+    cur_img[,,3] <- cur_img_CD44
+    
+    cur_img[is.na(cur_img)] <- 0
+    
+    expect_equal(Image(cur_out[[3]]), Image(cur_img, colormode = "Color"), tolerance = 0.03)
 })
 
 test_that("masks can be outlined by metadata.", {
