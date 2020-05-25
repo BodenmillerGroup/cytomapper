e67e5d61f70b74951532c204bd841056c168c866
nilseling <nils.eling@gmx.de>
2020-05-25 17:01:32 +0200

    Further shiny testing

--- R/shiny-server.R
+++ R/shiny-server.R
@@ -49,14 +49,28 @@
 }
 
 # Create scatter plots
-.createScatter <- function(object, markers, cur_sample, cur_assay, ranges, brush){
+.createScatter <- function(input, rValues, markers){
     renderPlot({
-        # Build data frame for visualization
-        cur_df <- as.data.frame(t(assay(object(), cur_assay())))
-        cur_df$sample <- cur_sample()
         
-        if(markers[[2]]() == ""){
+        req(rValues$object, rValues$ranges, input$assay, markers[[1]]())
+        
+        # Build data frame for visualization
+        cur_df <- as.data.frame(t(assay(rValues$object, input$assay)))
+        cur_df$sample <- input$sample
+        
+        if(markers[[2]]() != ""){
 
+            # Scatter plot    
+            ggplot(cur_df) +
+                geom_point(aes_(as.name(markers[[1]]()), as.name(markers[[2]]())), 
+                           show.legend = FALSE) +
+                ylab(markers[[2]]()) +
+                theme_minimal() + 
+                ylim(c(rValues$ranges[markers[[2]](), 1], rValues$ranges[markers[[2]](), 2])) +
+                xlim(c(rValues$ranges[markers[[1]](), 1], rValues$ranges[markers[[1]](), 2]))
+            
+        } else {
+            
             # Distributions of marker proteins
             ggplot(cur_df) +
                 geom_density(aes_(x = as.name(markers[[1]]())), 
@@ -64,30 +78,24 @@
                 theme(axis.text.y = element_blank(),
                       panel.background = element_blank()) +
                 ylab("") +
-                xlim(c(ranges()[markers[[1]](),1], ranges()[markers[[1]](),2]))
-            
-        } else {
-            ggplot(cur_df) +
-                geom_point(aes_(as.name(markers[[1]]()), as.name(markers[[2]]())), 
-                           show.legend = FALSE) +
-                ylab(markers[[2]]()) +
-                theme_minimal() + 
-                ylim(c(ranges()[markers[[2]](),1], ranges()[markers[[2]](),2])) +
-                xlim(c(ranges()[markers[[1]](),1], ranges()[markers[[1]](),2]))
+                xlim(c(rValues$ranges[markers[[1]](), 1], rValues$ranges[markers[[1]](), 2]))
         }
     })
 }
 
-.brushObject <- function(object, markers, cur_sample, cur_assay, brush){
+.brushObject <- function(input, rValues, markers, brush){
     
-    reactive({
+    observe({
+        
+        req(rValues$object, input$assay, markers[[1]](), brush())
+        
         # Build data frame 
-        cur_df <- as.data.frame(t(assay(object(), cur_assay())))
-        cur_df$sample <- cur_sample()
+        cur_df <- as.data.frame(t(assay(rValues$object, input$assay)))
+        cur_df$sample <- input$sample
     
         # Brush the data.frame
         cur_selection <- brushedPoints(cur_df, brush(), allRows = TRUE)
-    
+        
         # Save the Gate
         cur_gate <- list()
         cur_gate$gate <- matrix(data = c(brush()$xmin, brush()$xmax, 
@@ -95,20 +103,20 @@
                            nrow = 2, ncol = 2,
                            byrow = TRUE,
                            dimnames = list(c(markers[[1]](), markers[[2]]()), c("min", "max")))
-        cur_gate$exprs_values <- cur_assay()
-        cur_gate$img_id <- cur_sample()
+        cur_gate$exprs_values <- input$assay
+        cur_gate$img_id <- input$sample
     
         # Saved gates
-        all_gates <- names(metadata(object()))[grepl("cytomapper_gate", names(metadata(object())))]
+        all_gates <- names(metadata(rValues$object))[grepl("cytomapper_gate", names(metadata(rValues$object)))]
         if (is.null(all_gates)) {
-            metadata(object())$cytomapper_gate_1 <- cur_gate
+            metadata(rValues$object)$cytomapper_gate_1 <- cur_gate
         } else {
             cur_num <- as.numeric(vapply(all_gates, FUN = function(x){unlist(strsplit(x, split = "_"))[3]}, 
                                          FUN.VALUE = character(1)))
-            metadata(object())[[paste0("cytomapper_gate_", max(cur_num) + 1)]] <- cur_gate
+            metadata(rValues$object)[[paste0("cytomapper_gate_", max(cur_num) + 1)]] <- cur_gate
         }
-    
-        return(object()[,cur_selection$selected_])
+        
+        rValues$object <- rValues$object()[,cur_selection$selected_]
     })
     
 }
@@ -156,158 +164,135 @@
                          server = TRUE, selected = "")
     
     # Save some variables used throught the app
-    cur_sample <- reactive({input$sample})
-    cur_assay <- reactive({input$assay})
-    cur_rd <- reactive({input$reducedDim})
-    cur_ranges <- reactive({
-        cur_r <- rowRanges(assay(object, cur_assay()))
-        rownames(cur_r) <- rownames(object)
-        cur_r
-        })
-    cur_object <- reactive({object[,colData(object)[,img_id] == cur_sample()]})
+    rValues <- reactiveValues()
     
-    # Markers
-    cur_marker1 <- reactive({input$Marker_1})
-    cur_marker2 <- reactive({input$Marker_2})
-    cur_marker3 <- reactive({input$Marker_3})
-    cur_marker4 <- reactive({input$Marker_4})
-    cur_marker5 <- reactive({input$Marker_5})
-    cur_marker6 <- reactive({input$Marker_6})
+    observeEvent(input$sample, {
+        rValues$object <- object[,colData(object)[,img_id] == input$sample]
+    }, ignoreInit = TRUE)
     
-    # Brushes
-    brush1 <- reactive({input$plot_brush1})
-    brush2 <- reactive({input$plot_brush2})
-    brush3 <- reactive({input$plot_brush3})
+    observeEvent(input$assay, {
+        cur_ranges <- rowRanges(assay(object, input$assay))
+        rownames(cur_ranges) <- rownames(object)
+        rValues$ranges <- cur_ranges
+    }, ignoreInit = TRUE)
     
     # Session info observer
     cur_sessionInfo <- sessionInfo()
     .create_general_observer(input, si = cur_sessionInfo)
     
     # First scatter plot
-    output$scatter1 <- .createScatter(object = cur_object, 
-                                markers = list(cur_marker1, cur_marker2), 
-                                cur_sample = cur_sample,
-                                cur_assay = cur_assay,
-                                ranges = cur_ranges)
+    cur_marker1 <- reactive({input$Marker_1})
+    cur_marker2 <- reactive({input$Marker_2})
+    output$scatter1 <- .createScatter(input, rValues, 
+                            markers = list(cur_marker1, cur_marker2))
     
     # Brushing 1
-    cur_object1 <- .brushObject(object = cur_object,
-                                markers = list(cur_marker1, cur_marker2), 
-                                cur_sample = cur_sample,
-                                cur_assay = cur_assay,
-                                brush = brush1)
+    cur_brush1 <- reactive({input$plot_brush1})
+    .brushObject(input, rValues,
+                 markers = list(cur_marker1, cur_marker2), 
+                 brush = cur_brush1)
     
-    output$info1 <- renderText({
-        paste0(
-            "Selection: ", .brushRange(brush1())
-        )
-    })
+    #output$info1 <- renderText({
+    #    paste0(
+    #        "Selection: ", .brushRange(input$plot_brush1)
+    #    )
+    #})
     
     # Second scatter plot
-    observe({
-        if (!is.null(input$plot_brush1) && input$Marker_3 != "") {
-            output$scatter2 <- .createScatter(object = cur_object1, 
-                                              markers = list(cur_marker3, cur_marker4), 
-                                              cur_sample = cur_sample,
-                                              cur_assay = cur_assay,
-                                              ranges = cur_ranges)
-                
-            # Select SCE
-            cur_object2 <- .brushObject(object = cur_object1,
-                                        markers = list(cur_marker3, cur_marker4), 
-                                        cur_sample = cur_sample,
-                                        cur_assay = cur_assay,
-                                        brush = brush2)
-                
-            output$info2 <- renderText({
-                paste0(
-                    "Selection: ", .brushRange(input$plot_brush2)
-                )
-            })
-        }  
-    })
+    #observe({
+    #    if (!is.null(input$plot_brush1) && input$Marker_3 != "") {
+    #        output$scatter2 <- .createScatter(input, rValues, 
+    #                                          markers = c(input$Marker_3, input$Marker_4))
+    #            
+    #        # Select SCE
+    #        .brushObject(input, rValues,
+    #                     markers = c(input$Marker_3, input$Marker_4), 
+    #                     brush = input$plot_brush2)
+    #            
+    #        output$info2 <- renderText({
+    #            paste0(
+    #                "Selection: ", .brushRange(input$plot_brush2)
+    #            )
+    #        })
+    #    }  
+    #})
     
     # Third scatter plot
-    observe({
-        if (!is.null(input$plot_brush2) && input$Marker_5 != "") {
-            output$scatter3 <- .createScatter(object = cur_object2, 
-                                              markers = list(cur_marker5, cur_marker6), 
-                                              cur_sample = cur_sample,
-                                              cur_assay = cur_assay,
-                                              ranges = cur_ranges)
-            
-            # Select SCE
-            cur_object3 <- .brushObject(object = cur_object2,
-                                        markers = list(cur_marker5, cur_marker6), 
-                                        cur_sample = cur_sample,
-                                        cur_assay = cur_assay,
-                                        brush = brush2)
-            
-            output$info3 <- renderText({
-                paste0(
-                    "Selection: ", .brushRange(input$plot_brush3)
-                )
-            })
-        }  
-    })
+    #observe({
+    #    if (!is.null(input$plot_brush2) && input$Marker_5 != "") {
+    #        output$scatter3 <- .createScatter(input, rValues, 
+    #                                          markers = c(input$Marker_5, input$Marker_6))
+    #        
+    #        # Select SCE
+    #        .brushObject(input, rValues,
+    #                     markers = c(input$Marker_5, input$Marker_6), 
+    #                     brush = input$plot_brush3)
+    #        
+    #        output$info3 <- renderText({
+    #            paste0(
+    #                "Selection: ", .brushRange(input$plot_brush3)
+    #            )
+    #        })
+    #    }  
+    #})
     
     output$image_expression <- renderPlot({
         
-        if (cur_marker2 == "") {
-            cur_markers <- cur_marker1
-        } else {
-            cur_markers <- c(cur_marker1, cur_marker2)
-        }
+        #if (cur_marker2 == "") {
+        #    cur_markers <- cur_marker1
+        #} else {
+        #    cur_markers <- c(cur_marker1, cur_marker2)
+        #}
         
-        if (is.null(image)) {
-            cur_mask <- mask[mcols(mask)[,img_id] == cur_sample]
-            plotCells(object = cur_object,
-                      mask = cur_mask,
-                      cell_id = cell_id,
-                      img_id = img_id,
-                      colour_by = cur_markers,
-                      exprs_values = cur_assay,
-                      ...)
-        } else {
-            cur_image <- image[mcols(image)[,img_id] == sample]
-            plotPixels(image = cur_image,
-                       colour_by = cur_markers,
-                       ...)
-        }
+        #if (is.null(image)) {
+        #    cur_mask <- mask[mcols(mask)[,img_id] == cur_sample]
+        #    plotCells(object = cur_object,
+        #              mask = cur_mask,
+        #              cell_id = cell_id,
+        #              img_id = img_id,
+        #              colour_by = cur_markers,
+        #              exprs_values = cur_assay,
+        #              ...)
+        #} else {
+        #    cur_image <- image[mcols(image)[,img_id] == sample]
+        #    plotPixels(image = cur_image,
+        #               colour_by = cur_markers,
+        #               ...)
+        #}
         
     })
     
     output$image_selection <- renderPlot({
-        cur_object$selected <- TRUE
-        if(is.null(image)){
-            cur_mask <- mask[mcols(mask)[,img_id] == sample] 
-            plotCells(object = cur_object,
-                      mask = cur_mask,
-                      cell_id = cell_id,
-                      img_id = img_id,
-                      colour_by = "selected",
-                      colour = list(selected = c("TRUE" = "dark red", "FALSE" = "gray")),
-                      ...)
-        } else {
-            cur_mask <- mask[mcols(mask)[,img_id] == sample]
-            cur_image <- image[mcols(image)[,img_id] == sample]
-            plotPixels(image = cur_image,
-                      object = cur_object,
-                      mask = cur_mask,
-                      cell_id = cell_id,
-                      img_id = img_id,
-                      colour_by = "selected",
-                      colour = list(selected = c("TRUE" = "dark red", "FALSE" = "gray")),
-                      ...)
-        }
-        cur_object$selected <- NULL
+        #cur_object$selected <- TRUE
+        #if(is.null(image)){
+        #    cur_mask <- mask[mcols(mask)[,img_id] == sample] 
+        #    plotCells(object = cur_object,
+        #              mask = cur_mask,
+        #              cell_id = cell_id,
+        #              img_id = img_id,
+        #              colour_by = "selected",
+        #              colour = list(selected = c("TRUE" = "dark red", "FALSE" = "gray")),
+        #              ...)
+        #} else {
+        #    cur_mask <- mask[mcols(mask)[,img_id] == sample]
+        #    cur_image <- image[mcols(image)[,img_id] == sample]
+        #    plotPixels(image = cur_image,
+        #              object = cur_object,
+        #              mask = cur_mask,
+        #              cell_id = cell_id,
+        #              img_id = img_id,
+        #              colour_by = "selected",
+        #              colour = list(selected = c("TRUE" = "dark red", "FALSE" = "gray")),
+        #              ...)
+        #}
+        #cur_object$selected <- NULL
     })
 
     output$downloadData <- downloadHandler(
-        filename = "gated_sce.rds",
-        content = function(filename) {
-            saveRDS(cur_object, filename)
-        }
+        #filename = "gated_sce.rds",
+        #content = function(filename) {
+        #    saveRDS(cur_object, filename)
+        #}
     )
 }
 
