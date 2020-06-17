81d305418adae18ff4b10875e3b535f71209fd42
Tobias Hoch <tobias.hoch@bluewin.ch>
2020-06-17 14:51:05 +0200

    split dashboardBody to two tabs

--- R/shiny-ui.R
+++ R/shiny-ui.R
@@ -85,28 +85,62 @@
 
 # Create the main body
 #' @importFrom shinydashboard dashboardBody
+
+body <- dashboardBody(
+    fluidRow(
+        tabBox(
+            title = "First tabBox",
+            # The id lets us use input$tabset1 on the server to find the current tab
+            id = "tabset1", height = "250px",
+            tabPanel("Tab1", "First tab content"),
+            tabPanel("Tab2", "Tab content 2")
+        ),
+        tabBox(
+            side = "right", height = "250px",
+            selected = "Tab3",
+            tabPanel("Tab1", "Tab content 1"),
+            tabPanel("Tab2", "Tab content 2"),
+            tabPanel("Tab3", "Note that when side=right, the tab order is reversed.")
+        )
+    ),
+    fluidRow(
+        tabBox(
+            # Title can include an icon
+            title = tagList(shiny::icon("gear"), "tabBox status"),
+            tabPanel("Tab1",
+                     "Currently selected tab from first box:",
+                     verbatimTextOutput("tabset1Selected")
+            ),
+            tabPanel("Tab2", "Tab content 2")
+        )
+    )
+)
+
 .cytomapper_body <- function(){
     cm_body <- dashboardBody(
-        uiOutput("AdditionalPlots_main"),
-        fluidRow(box(column(width = 6, selectizeInput("exprs_marker_1",
-                                    label = span(paste("Select marker 1"), 
-                                               style = "color: black; padding-top: 0px"), 
-                                    choices = NULL,
-                                    options = list(placeholder = '', 
-                                             maxItems = 1))),
-                     column(width = 6, selectizeInput("exprs_marker_2",
-                                    label = span(paste("Select marker 2"), 
-                                                 style = "color: black; padding-top: 0px"), 
-                                    choices = NULL,
-                                    options = list(placeholder = '', 
-                                                   maxItems = 1))),
-                     plotOutput("image_expression", height = "300px"), 
-                     title = "Expression", status = "primary",
-                     width = 6, height = "500px"),
-                 box(plotOutput("image_selection"), 
-                     title = "Selection", status = "primary",
-                     width = 6, height = "500px"))
+        tabBox(width = 12,
+            tabPanel(title = "Scatter Plots", uiOutput("AdditionalPlots_main")),
+            tabPanel(title = "Images", fluidRow(box(column(width = 6, selectizeInput("exprs_marker_1",
+                                                                                          label = span(paste("Select marker 1"), 
+                                                                                          style = "color: black; padding-top: 0px"), 
+                                                                                          choices = NULL,
+                                                                                          options = list(placeholder = '', 
+                                                                                                         maxItems = 1))),
+                                                         column(width = 6, selectizeInput("exprs_marker_2",
+                                                                                          label = span(paste("Select marker 2"), 
+                                                                                                       style = "color: black; padding-top: 0px"), 
+                                                                                          choices = NULL,
+                                                                                          options = list(placeholder = '', 
+                                                                                                         maxItems = 1))),
+                                                         plotOutput("image_expression", height = "300px"), 
+                                                         title = "Expression", status = "primary",
+                                                         width = 6, height = "500px"),
+                                                     box(svgPanZoomOutput("image_selection"), 
+                                                         title = "Selection", status = "primary",
+                                                         width = 6, height = "500px")))
+        )
         )
     return(cm_body)
     }
+
 
