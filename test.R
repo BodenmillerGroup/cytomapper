842a43c33febdcfae6ee94ff4f61f5f4013b97a8
Tobias Hoch <tobias.hoch@bluewin.ch>
2020-06-19 12:09:17 +0200

    update test

--- test.R
+++ test.R
@@ -1,9 +1,7 @@
 devtools::load_all("/Volumes/thoch/Git/cytomapper/")
-
 data(pancreasSCE)
 data(pancreasImages)
 data(pancreasMasks)
-
 
 cytomapperShiny(object = pancreasSCE, mask = pancreasMasks, image = pancreasImages,
                         cell_id = "CellNb", img_id = "ImageNb")
