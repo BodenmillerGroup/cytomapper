9afcb526dcce657afbe7ae5a1f8bc8d94db23351
Tobias Hoch <tobias.hoch@bluewin.ch>
2020-06-19 12:08:07 +0200

    update UI with zoom function and contrast input

--- R/shiny-ui.R
+++ R/shiny-ui.R
@@ -86,58 +86,36 @@
 # Create the main body
 #' @importFrom shinydashboard dashboardBody
 
-body <- dashboardBody(
-    fluidRow(
-        tabBox(
-            title = "First tabBox",
-            # The id lets us use input$tabset1 on the server to find the current tab
-            id = "tabset1", height = "250px",
-            tabPanel("Tab1", "First tab content"),
-            tabPanel("Tab2", "Tab content 2")
-        ),
-        tabBox(
-            side = "right", height = "250px",
-            selected = "Tab3",
-            tabPanel("Tab1", "Tab content 1"),
-            tabPanel("Tab2", "Tab content 2"),
-            tabPanel("Tab3", "Note that when side=right, the tab order is reversed.")
-        )
-    ),
-    fluidRow(
-        tabBox(
-            # Title can include an icon
-            title = tagList(shiny::icon("gear"), "tabBox status"),
-            tabPanel("Tab1",
-                     "Currently selected tab from first box:",
-                     verbatimTextOutput("tabset1Selected")
-            ),
-            tabPanel("Tab2", "Tab content 2")
-        )
-    )
-)
-
 .cytomapper_body <- function(){
     cm_body <- dashboardBody(
-        tabBox(width = 12,
-            tabPanel(title = "Scatter Plots", uiOutput("AdditionalPlots_main")),
-            tabPanel(title = "Images", fluidRow(box(column(width = 6, selectizeInput("exprs_marker_1",
+        tabBox(width = 12, id = "tabbox1",
+            tabPanel(title = "Scatter Plots", value = "tab1", uiOutput("AdditionalPlots_main")),
+            tabPanel(title = "Images", value = "tab2", fluidRow(box(column(width = 6, selectizeInput("exprs_marker_1",
                                                                                           label = span(paste("Select marker 1"), 
                                                                                           style = "color: black; padding-top: 0px"), 
                                                                                           choices = NULL,
                                                                                           options = list(placeholder = '', 
-                                                                                                         maxItems = 1))),
+                                                                                                         maxItems = 1)),
+                                                                           numericInput("contrast_marker_1",
+                                                                        label = span(paste("Contrast Marker 1"), 
+                                                                                     style = "color: black; padding-top: 0px"),
+                                                                       value = 1)),
                                                          column(width = 6, selectizeInput("exprs_marker_2",
                                                                                           label = span(paste("Select marker 2"), 
                                                                                                        style = "color: black; padding-top: 0px"), 
                                                                                           choices = NULL,
                                                                                           options = list(placeholder = '', 
-                                                                                                         maxItems = 1))),
-                                                         plotOutput("image_expression", height = "300px"), 
+                                                                                                         maxItems = 1)),
+                                                                numericInput("contrast_marker_2",
+                                                                             label = span(paste("Contrast Marker 2"), 
+                                                                                          style = "color: black; padding-top: 0px"),
+                                                                             value = 1)),
+                                                    svgPanZoomOutput("image_expression", height = "300px"), 
                                                          title = "Expression", status = "primary",
-                                                         width = 6, height = "500px"),
+                                                         width = 6, height = "550px"),
                                                      box(svgPanZoomOutput("image_selection"), 
                                                          title = "Selection", status = "primary",
-                                                         width = 6, height = "500px")))
+                                                         width = 6, height = "550px")))
         )
         )
     return(cm_body)
