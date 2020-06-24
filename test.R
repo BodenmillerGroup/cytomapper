de0997bebc99f3fc5126fc6328e5ce931a31db9b
toobiwankenobi <38858452+toobiwankenobi@users.noreply.github.com>
2020-06-24 15:57:17 +0200

    delete test.Rmd file

--- test.R
+++
@@ -1,8 +0,0 @@
-devtools::load_all("/Volumes/thoch/Git/cytomapper/")
-data(pancreasSCE)
-data(pancreasImages)
-data(pancreasMasks)
-
-cytomapperShiny(object = pancreasSCE, mask = pancreasMasks, image = pancreasImages,
-                        cell_id = "CellNb", img_id = "ImageNb")
-
