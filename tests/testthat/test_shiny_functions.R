87ed6bf8a467ddc16cad308d8c310a058d192b1c
nilseling <nils.eling@gmx.de>
2020-07-15 17:28:38 +0200

    First shiny unit test

---
+++ tests/testthat/test_shiny_functions.R
@@ -0,0 +1,37 @@
+test_that("cytomapperShiny: Standard input testing works", {
+    data("pancreasSCE")
+    data("pancreasMasks")
+    data("pancreasImages")
+    
+    # Fail
+    expect_error(cytomapperShiny(object = "test"), 
+                 regexp = "'object' is not of type 'SingleCellExperiment'.",
+                 fixed = TRUE)
+    expect_error(cytomapperShiny(object = pancreasSCE), 
+                 regexp = "Please provide an 'img_id' argument.")
+    expect_error(cytomapperShiny(object = pancreasSCE, img_id = c("test", 2)), 
+                 regexp = "Invalid argument for 'img_id'.",
+                 fixed = TRUE)
+    expect_error(cytomapperShiny(object = pancreasSCE, img_id = 2), 
+                 regexp = "Invalid argument for 'img_id'.",
+                 fixed = TRUE)
+    expect_error(cytomapperShiny(object = pancreasSCE, img_id = "test"), 
+                 regexp = "'img_id' not in 'colData(object)'.",
+                 fixed = TRUE)
+    cur_obj <- pancreasSCE
+    rownames(cur_obj) <- NULL
+    expect_error(cytomapperShiny(object = cur_obj, img_id = "ImageNb"), 
+                 regexp = "Please specify the rownames of the 'object'.",
+                 fixed = TRUE)
+    rownames(cur_obj) <- paste0("test", 1:5)
+    expect_error(cytomapperShiny(object = cur_obj, image = pancreasImages, img_id = "ImageNb"), 
+                 regexp = "The 'channelNames' of the images need to match the rownames of the object.",
+                 fixed = TRUE)
+    cur_images <- pancreasImages
+    channelNames(cur_images) <- NULL
+    expect_error(cytomapperShiny(object = pancreasSCE, image = cur_images, img_id = "ImageNb"), 
+                 regexp = "Please specify the 'channelNames' of the 'image' object.",
+                 fixed = TRUE)
+    
+    # Pass
+})
