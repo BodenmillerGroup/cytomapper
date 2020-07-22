aa82a831ca1ff0cda45a98493bc03bb8442e7895
Tobias Hoch <tobias.hoch@bluewin.ch>
2020-07-22 17:29:08 +0200

    add reactive input/output testing

--- tests/testthat/test_shiny_reactive.R
+++ tests/testthat/test_shiny_reactive.R
@@ -1,10 +1,23 @@
-data("pancreasImages")
-data("pancreasMasks")
-data("pancreasSCE")
-
-testServer(app = cytomapperShiny(object = pancreasSCE, img_id = "ImageNb"), {
-    session$setInputs(sample = 1,
-                      plotCount = 1)
-    expect_equal(input$sample, 1)
-    
-})
+test_that("test reactive input and output", {
+    data("pancreasImages")
+    data("pancreasMasks")
+    data("pancreasSCE")
+    testServer(app = cytomapperShiny(object = pancreasSCE, img_id = "ImageNb", 
+                                     cell_id = "CellNb", mask = pancreasMasks, image = pancreasImages), {
+                                         session$setInputs(sample = 1,
+                                                           plotCount = 1,
+                                                           assay = "counts",
+                                                           exprs_marker_1 = "H3",
+                                                           exprs_marker_2 = "CD99",
+                                                           contrast_marker_1 = 1,
+                                                           contrast_marker_2 = 1)
+                                         expect_equal(input$sample, 1)
+                                         expect_equal(input$plotCount, 1)
+                                         expect_equal(input$assay, "counts")
+                                         expect_equal(input$exprs_marker_1, "H3")
+                                         expect_equal(input$exprs_marker_2, "CD99")
+                                         expect_equal(input$contrast_marker_1, 1)
+                                         expect_equal(input$contrast_marker_2, 1)
+                                     })
+}
+)
