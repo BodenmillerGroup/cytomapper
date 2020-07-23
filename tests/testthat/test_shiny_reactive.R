9c91c5058560de13f88c5d2ddbe82a77d0cb7670
nilseling <nils.eling@gmx.de>
2020-07-23 09:18:15 +0200

    Explicitly call testServer from shiny

--- tests/testthat/test_shiny_reactive.R
+++ tests/testthat/test_shiny_reactive.R
@@ -2,9 +2,9 @@ data("pancreasImages")
 data("pancreasMasks")
 data("pancreasSCE")
 
-testServer(app = cytomapperShiny(object = pancreasSCE, img_id = "ImageNb", cell_id = "CellNb"), {
+shiny::testServer(app = cytomapperShiny(object = pancreasSCE, img_id = "ImageNb", cell_id = "CellNb"), {
     session$setInputs(sample = 1,
                       plotCount = 1)
     expect_equal(input$sample, 1)
-    
+
 })
