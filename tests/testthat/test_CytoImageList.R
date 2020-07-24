eb39c6270fcceb05d88c14b56a548b9fe4905be8
nilseling <nils.eling@gmx.de>
2020-07-24 17:10:01 +0200

    Added show function test

--- tests/testthat/test_CytoImageList.R
+++ tests/testthat/test_CytoImageList.R
@@ -20,3 +20,22 @@ test_that("Images can be loaded into CytoImageList object.", {
   cur_list[[1]] <- cur_list[[1]][,,-1]
   expect_error(CytoImageList(cur_list))
 })
+
+test_that("Show function works.", {
+    data(pancreasImages)
+    
+    test <- capture.output(show(pancreasImages))
+    expect_length(test, 4)
+    expect_equal(test[1], "CytoImageList containing 3 image(s)")
+    expect_equal(test[2], "names(3): E34_imc G01_imc J02_imc ")
+    expect_equal(test[3], "Each image contains 5 channel(s)")
+    expect_equal(test[4], "channelNames(5): H3 CD99 PIN CD8a CDH ")
+    
+    data(pancreasMasks)
+    
+    test <- capture.output(show(pancreasMasks))
+    expect_length(test, 3)
+    expect_equal(test[1], "CytoImageList containing 3 image(s)")
+    expect_equal(test[2], "names(3): E34_mask G01_mask J02_mask ")
+    expect_equal(test[3], "Each image contains 1 channel")
+})
