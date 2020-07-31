5ee3d40d3b323ae034e8fa59c95713442223d4df
toobiwankenobi <tobias.hoch@bluewin.ch>
2020-07-31 12:54:39 +0200

    merge conflict

--- R/cytomapperShiny.R
+++ R/cytomapperShiny.R
@@ -1,84 +1,3 @@
-<<<<<<< HEAD
-#' Shiny application to visualise gated cells on images
-#'
-#' This shiny application allows users to gate cells based on their raw or
-#' transformed expression values and visualises gated cells on their
-#' corresponding images.
-#'
-#' @param object a \code{\linkS4class{SingleCellExperiment}} object.
-#' @param mask (optional) a \code{\linkS4class{CytoImageList}} containing
-#'   single-channel \code{\linkS4class{Image}} objects
-#' @param image (optional) a \code{\linkS4class{CytoImageList}} object
-#'   containing single or multi-channel \code{\linkS4class{Image}} objects.
-#' @param cell_id character specifying the \code{colData(object)} entry, in
-#'   which the integer cell IDs are stored. These IDs should match the integer
-#'   pixel values in the segmentation mask object.
-#' @param img_id character specifying the \code{colData(object)} and
-#'   \code{mcols(mask)} and/or \code{mcols(image)} entry, in which the image IDs
-#'   are stored.
-#' @param ... parameters passed to the \code{\link{plotCells}} or
-#'   \code{\link{plotPixels}} function.
-#'
-#' @section User inputs: 
-#' This function requires at least a \code{\linkS4class{SingleCellExperiment}}
-#' input object. Gating is performed on cell-specific marker counts stored in
-#' the \code{assay} slots of the object. These can either be raw counts (usually
-#' stored in the \code{counts} slot) or transformed/scaled counts stored in
-#' other assay slots. Gating can only be performed sample-wise; therefore, even 
-#' if \code{mask} or \code{image} are not specified, \code{img_id} needs to point to
-#' the \code{colData} entry storing unique sample IDs.
-#' 
-#' If mask is specified
-#'
-#' @section The user interface
-#' The UI is based on two tabs ("Scatter Plots" and "Images") and a "General Control" panel. Using the control panel, the user
-#' can set the number of scatter plots to define a cell population, on which sample the gating should be applied, and which \code{Assay}
-#' should be displayed. Depending on the number of plots selected, the user can define the marker that are used for 
-#' each plot. At least one marker has to be defined (displayed as jittered points), two markers are shown as scatter plot. 
-#'
-#' @section Download of gated cells:
-#' After a set of cells are selected the user can downloaded the cell data in 
-#' the form of a \code{\linkS4class{SingleCellExperiment}} object stored in a RDS file. The object is a subset of the input 
-#' \code{\linkS4class{SingleCellExperiment}} object. The object contains a new column named 
-#' \code{CellLabel} stored in the \code{colData} of the \code{\linkS4class{SingleCellExperiment}} object. The new column
-#' contains a character label defined by the user.
-#'
-#' @section Getting further help:
-#' Find more information by clicking on the question mark in the upper right corner. 
-#'
-#' @seealso \code{\link{plotCells}} and \code{\link{plotPixels}} for the main plotting function
-#'
-#' @examples \code{
-#' # Only run this example in interactive R sessions
-#' if (interactive()) {
-#'    options(device.ask.default = FALSE)
-#' # Load example data sets
-#' data("pancreasSCE")
-#' data("pancreasImages")
-#' data("pancreasMasks")
-#' 
-#' # Use shiny with SCE object, images and masks
-#' cytomapperShiny(pancreasSCE, pancreasMasks, pancreasImages, 
-#' cell_id = "CellNb", img_id = "ImageNb")
-#' 
-#' # Use shiny with SCE object and masks
-#' cytomapperShiny(pancreasSCE, pancreasMasks, 
-#' cell_id = "CellNb", img_id = "ImageNb")
-#' 
-#' # Use shiny with SCE object only
-#' cytomapperShiny(pancreasSCE, 
-#' cell_id = "CellNb", img_id = "ImageNb")
-#' }
-#'}
-#'
-#' @author Nils Eling (\email{nils.eling@@dqbm.uzh.ch})
-#' @author Tobias Hoch (\email{tobias.hoch@@dqbm.uzh.ch})
-#'
-#' @export
-#'
-#' @import shiny
-#' @import shinydashboard
-=======
 #'Shiny application to visualise gated cells on images
 #'
 #'This shiny application allows users to gate cells based on their raw or
@@ -206,7 +125,7 @@
 #'
 #'@import shiny
 #'@import shinydashboard
->>>>>>> master
+
 cytomapperShiny <- function(object,
                         mask = NULL,
                         image = NULL,
