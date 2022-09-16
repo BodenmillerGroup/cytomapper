0e7f2e4d67e53d54d0fd42f0aaacfb7f5beae52c
nilseling <nils.eling@gmx.de>
2022-09-16 14:08:44 +0200

    Removed CytoImageList class

--- R/AllClasses.R
+++
@@ -1,8 +0,0 @@
-# -------------------------------------------------------------------------
-# The Image_OR_DelayedArray class
-# -------------------------------------------------------------------------
-
-#' @importClassesFrom HDF5Array HDF5Array
-#' @importClassesFrom DelayedArray DelayedArray
-#' @importFrom EBImage Image 
-setClassUnion("Image_OR_DelayedArray", c("Image", "DelayedArray", "HDF5Array"))
