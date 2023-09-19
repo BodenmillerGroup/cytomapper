cd69d04c3b2a5254911080841c5f35fed0d3e79f
lassedochreden <lasse.meyer@uzh.ch>
2023-09-19 13:02:43 +0200

    start masks reader

--- R/ZarrReaders.R
+++ R/ZarrReaders.R
@@ -20,12 +20,13 @@
 #' @examples
 #' # TODO
 #'
-#' @author Nils Eling (\email{nils.eling@@dqbm.uzh.ch}),
+#' @author Lasse Meyer (\email{lasse.meyer@@dqbm.uzh.ch}),
 #'
 #' @export
 #' @importFrom jsonlite fromJSON
 #' @importFrom Rarr read_zarr_array zarr_overview
-readImagesFromZARR <- function(file,
+
+readMasksFromZARR <- function(file,
                      type = c("omengff", "spatialdata"),
                      resolution = NULL,
                      x = NULL,
@@ -39,48 +40,52 @@ readImagesFromZARR <- function(file,
     #.valid.readZARR.input(file)
     
     type <- match.arg(type)
-    what <- match.arg(what)
+
+    #browser()
     
     if (type == "spatialdata") {
         if (is.null(fov_names)) {
-            fov_names <- list.files(file.path(file, "images"))[1]
+            fov_names <- list.files(file.path(file, "labels"))[1]
         }
         
         if (is.null(resolution)) {
             if (length(fov_names) > 1) {
-                resolution <- lapply(file.path(file, "images", fov_names), 
+                resolution <- lapply(file.path(file, "labels", fov_names), 
                                      function(cur_name) {
                                          cur_res <- list.files(cur_name)
                                          return(cur_res[length(cur_res)])
                                      })
                 resolution <- unlist(resolution)
             } else {
-                resolution <- list.files(file.path(file, "images", fov_names))
+                resolution <- list.files(file.path(file, "labels", fov_names))
                 resolution <- resolution[length(resolution)]
             }
         }
         
-        cur_names <- file.path(file, "images", fov_names, resolution)
+        cur_names <- file.path(file, "labels", fov_names, resolution)
         
         # Read in metadata
-        cur_meta <- lapply(file.path(file, "images", fov_names),
+        cur_meta <- lapply(file.path(file, "labels", fov_names),
                            function(cur_m) {
                                return(fromJSON(file.path(cur_m, ".zattrs")))
                            })
         
-        cur_channels <- lapply(cur_meta, function(cur_m){
-            if ("omero" %in% names(cur_meta)) {
-                return(cur_meta$omero$channels$label)
-            } else if ("channels_metadata" %in% names(cur_meta)) {
-                return(cur_meta$channels_metadata$channels$label)
-            } 
+        cur_index <- lapply(cur_meta, function(cur_m){
+          if ("coordinateTransformations" %in% names(cur_m$multiscales)) {
+            return(cur_m$multiscales$coordinateTransformations[[1]]$input$axes[[1]]$name)
+          } else {
+            return(cur_m$multiscales$axes[[1]]$name)
+          } 
         })
         
-        if (length(unique(cur_channels)) > 1) {
-            stop("Channel names need to match across images.")
+        if (length(unique(cur_index)) > 1) {
+          stop("Dimensions need to match across images.")
         }
         
+        cur_index <- unlist(unique(cur_index))
+        
     } else if (type == "omengff") {
+      
         if (is.null(resolution)) {
             resolution <- list.files(file, pattern = paste(seq(0, 10), collapse = "|"))
             resolution <- resolution[length(resolution)]
@@ -92,26 +97,20 @@ readImagesFromZARR <- function(file,
         
         cur_meta <- fromJSON(file.path(file, ".zattrs"))
         
-        if ("omero" %in% names(cur_meta)) {
-            cur_channels <- cur_meta$omero$channels$label
-        } else if ("channels_metadata" %in% names(cur_meta)) {
-            cur_channels <- cur_meta$channels_metadata$channels$label
-        } 
+        # if ("omero" %in% names(cur_meta)) {
+        #     cur_channels <- cur_meta$omero$channels$label
+        # } else if ("channels_metadata" %in% names(cur_meta)) {
+        #     cur_channels <- cur_meta$channels_metadata$channels$label
+        # } 
     }
     
+    message("Resolution of the image: ", resolution)
+
     cur_out <- lapply(cur_names, function(cur_name){
         
-        # Create index
-        cur_zarr <- zarr_overview(cur_name, as_data_frame = TRUE)
-        cur_dim <- cur_zarr$dim[[1]]
-        
-        if (length(cur_dim) == 3) {
-            cur_ind <- list(c, y, x)
-        } else if (length(cur_dim) == 4) {
-            cur_ind <- list(z, c, y,  x)
-        } else if (length(cur_dim) == 5) {
-            cur_ind <- list(t, z, c, y, x)
-        }
+        cur_ind <- list(t, c, z, y, x)
+        names(cur_ind) <-  c("t", "c", "z", "y", "x")
+        cur_ind <- cur_ind[cur_index]
         
         cur_arr <- read_zarr_array(cur_name, index = cur_ind)
         cur_arr <- Image(aperm(cur_arr, rev(seq_along(dim(cur_arr)))))
@@ -119,16 +118,11 @@ readImagesFromZARR <- function(file,
         return(cur_arr)
     })
     
+    #browser()
+    
     names(cur_out) <- fov_names
     
     cur_CIL <- CytoImageList(cur_out)
-    
-    # Set channelNames
-    if (exists("cur_channels")) {
-        channelNames(cur_CIL) <- cur_channels
-    } else {
-        channelNames(cur_CIL) <- as.character(seq_len(dim(cur_CIL[[1]])[3]))
-    }
     
     return(cur_CIL)
 }
